1302b27da5e474f4c0e93ff7e250630d383b6077
Jarriola989 <55008708+Jarriola989@users.noreply.github.com>
2019-09-12 01:17:13 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,9 @@
+function addPayment()
+{
+    document.getElementById("hiddenpayment").style.display = 'block';
+}
+
+function addShipping()
+{
+    document.getElementById("hiddenshipping").style.display = 'block';
+}
