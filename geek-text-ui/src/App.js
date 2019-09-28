2f68ab9e2ee27bab952f8cc7e95a6f7744b64795
Roland <roland.aroche@gmail.com>
2019-09-27 23:40:35 -0400

    Installed react-bootstrap

--- geek-text-ui/src/App.js
+++ geek-text-ui/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <div className="App">
         <h1>Geek Text App - App.js</h1>
       </div>
