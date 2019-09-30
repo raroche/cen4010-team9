b4bfad149c85c3e51553548987bb2bc77ee68947
Jarriola989 <55008708+Jarriola989@users.noreply.github.com>
2019-09-30 00:32:44 -0400

    Delete profile.js

--- profile.js
+++
@@ -1,9 +0,0 @@
-function addPayment()
-{
-    document.getElementById("hiddenpayment").style.display = 'block';
-}
-
-function addShipping()
-{
-    document.getElementById("hiddenshipping").style.display = 'block';
-}
