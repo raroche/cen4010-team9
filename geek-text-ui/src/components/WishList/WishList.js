060b45feace3937ddd31f4b4d0a7213dad6cd8ec
Samuel Yorizzo <syori001@fiu.edu>
2019-10-05 01:11:25 -0400

    Added a header for recent releases for the homepage, wishlist using a mock database must implement delete list, change between list, move to cart and improve the ui

---
+++ geek-text-ui/src/components/WishList/WishList.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import { Card, Button } from "react-bootstrap";
+import Toread from "./Books";
+import Dystopian from "./Books2";
+import Navbar from "./Navbar";
+
+function ListCard(props) {
+  return (
+    <div>
+      <Card
+        className="p-3"
+        style={{ top: "40px", width: "900px", left: "500px" }}
+      >
+        <Card.Body>
+          <img
+            src={props.cover}
+            height="150"
+            width="100"
+            padding="20px"
+            alt="book cover"
+          />
+          <Button
+            variant="primary"
+            style={{ position: "absolute", right: "50px", top: "90px" }}
+          >
+            Move to cart
+          </Button>
+          <Card.Text
+            className="text-center"
+            style={{ position: "absolute", top: "70px", left: "200px" }}
+          >
+            <h1 style={{ fontweight: "bold" }}>{props.bookName} </h1>
+            {props.author}
+            <br />
+            <p1 style={{ left: "200px" }}> {props.price} </p1>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+class WishList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Lists: [Toread, Dystopian],
+      books: Toread
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(props) {
+    console.log(props);
+    if (props === "Create List") {
+      let temp = this.state.Lists;
+      const newElement = [
+        {
+          id: 1,
+          Listid: 3,
+          ListName: "Test",
+          bookName: "The Martian",
+          author: "Andy Weir",
+          price: "14.95",
+          cover:
+            "https://images-na.ssl-images-amazon.com/images/I/81wFMY9OAFL.jpg"
+        },
+        {
+          id: 2,
+          Listid: 3,
+          bookName: "1984",
+          author: "George Orwell",
+          price: "9.95",
+          cover:
+            "https://images-na.ssl-images-amazon.com/images/I/51Mn9-I8CZL._SX322_BO1,204,203,200_.jpg"
+        }
+      ];
+      temp.push(newElement);
+      console.log(temp);
+      this.setState({
+        Lists: temp,
+        books: newElement
+      });
+    } else if (props === this.state.Lists[0][0].Listid) {
+      console.log(1);
+      this.setState({
+        books: this.state.Lists[0]
+      });
+    } else if (props === this.state.Lists[1][0].Listid) {
+      this.setState({
+        books: this.state.Lists[1]
+      });
+    } else if (props === this.state.Lists[2][0].Listid) {
+      this.setState({
+        books: this.state.Lists[2]
+      });
+    }
+  }
+
+  render() {
+    let listCard = this.state.books.map(item => (
+      <ListCard
+        key={item.id}
+        author={item.author}
+        cover={item.cover}
+        price={item.price}
+        bookName={item.bookName}
+      />
+    ));
+    return (
+      <div>
+        <Navbar
+          Lists={this.state.Lists}
+          handleClick={this.handleClick}
+          default={this.state.Lists[0].ListName}
+        />
+
+        {listCard}
+      </div>
+    );
+  }
+}
+
+export default WishList;
