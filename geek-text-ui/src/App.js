1437f290228ebfade1d5c2d291314aae55178607
Roland <roland.aroche@gmail.com>
2019-10-05 22:35:29 -0400

    Janeth merge resolved

--- geek-text-ui/src/App.js
+++ geek-text-ui/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import CreateAccount from './components/CreateAccount/CreateAccount';
-import LoginRegister from './components/LoginRegister/LoginRegister';
-import HomePage from './components/HomePage/HomePage';
+import CreateAccount from "./components/CreateAccount/CreateAccount";
+import LoginRegister from "./components/LoginRegister/LoginRegister";
+import HomePage from "./components/HomePage/HomePage";
 import "./App.css";
 import BooksGrid from "./components/BooksGrid/BooksGrid";
 import BookDetails from "./components/BookDetails/BookDetails";
-import Header from './components/header/Header';
+import Header from "./components/header/Header";
 import Cart from "./components/Cart/Cart";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -13,19 +13,14 @@ import { Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>      
-        <div style={{height:'100%'}}>
-          <Header />
-            <main>  
-              <div style={{height: '50px'}}></div>
-              <Route exact path="/" render={props =>(<HomePage />)} />
-              <Route path="/createaccount" component={CreateAccount} />
-              <Route path="/login" component={LoginRegister} />
-              <Route path="/bookgrid" component={BooksGrid} />
-              <Route path="/book" component={BookDetails} />
-              <Route path="/cart" component = {Cart}/>
-            </main>
-        </div> 
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" render={props => <HomePage />} />
+        <Route path="/createaccount" component={CreateAccount} />
+        <Route path="/login" component={LoginRegister} />
+        <Route path="/bookgrid" component={BooksGrid} />
+        <Route path="/book" component={BookDetails} />
+        <Route path="/cart" component={Cart} />
       </BrowserRouter>
     );
   }
