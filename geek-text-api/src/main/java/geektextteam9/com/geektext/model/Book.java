dba91727398e743a5556e4bf09982880052973e2
Roland <roland.aroche@gmail.com>
2019-10-08 19:02:25 -0400

    worked on book class, review class, and it relations

--- geek-text-api/src/main/java/geektextteam9/com/geektext/model/Book.java
+++ geek-text-api/src/main/java/geektextteam9/com/geektext/model/Book.java
@@ -1,6 +1,8 @@
 package geektextteam9.com.geektext.model;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.util.List;
 
 @Entity
 public class Book {
@@ -18,6 +20,98 @@ public class Book {
 
     @Column
     private String description;
+
+    @Column
+    private String price;
+
+    @Column
+    private Date date;
+
+    @Column
+    private float rating;
+
+    @Column
+    private String img_url;
+
+    @Column
+    private boolean top_seller;
+
+    @Column
+    private boolean featured;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    public Book(String isbn, String title, String description, String price, Date date, float rating,
+                    String img_url, boolean top_seller, boolean featured, List<Review> reviews) {
+        this.isbn = isbn;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.date = date;
+        this.rating = rating;
+        this.img_url = img_url;
+        this.top_seller = top_seller;
+        this.featured = featured;
+        this.reviews = reviews;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public boolean isTop_seller() {
+        return top_seller;
+    }
+
+    public void setTop_seller(boolean top_seller) {
+        this.top_seller = top_seller;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
 
 
 
---
+++ geek-text-api/src/main/java/geektextteam9/com/geektext/model/Review.java
@@ -0,0 +1,60 @@
+package geektextteam9.com.geektext.model;
+
+import javax.persistence.*;
+
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private Integer id;
+
+    @Column
+    private float rating;
+
+    @Column
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn // Indicate the Foreign key in in this table
+    @JoinColumn(name="book_id")
+    private Book book;
+
+    public Review(float rating, String comment, Book book) {
+        this.rating = rating;
+        this.comment = comment;
+        this.book = book;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
