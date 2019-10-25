8269f19e13284acab66b482c41e0424a04be0853
Roland <roland.aroche@gmail.com>
2019-10-25 01:11:46 -0400

    API endpoint created to get the featured books

---
+++ geek-text-api/src/main/java/geektextteam9/com/geektext/ws/HomeRestController.java
@@ -0,0 +1,26 @@
+package geektextteam9.com.geektext.ws;
+
+
+import geektextteam9.com.geektext.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/")
+public class HomeRestController {
+
+    @Autowired
+    private HomeService homeService;
+
+    @GetMapping(path = "/")
+    public ResponseEntity<List> findByFeatured() {
+        return ResponseEntity.ok(homeService.findAllByFeatured());
+    }
+}
