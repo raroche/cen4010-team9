b88c1418e8a3a52cdd34a7a1993a71c6c44c9d7a
Roland <roland.aroche@gmail.com>
2020-01-17 23:50:35 -0500

    App Builded using npm run build

--- geek-text-ui/src/components/SignIn/SignIn.js
+++ geek-text-ui/src/components/SignIn/SignIn.js
@@ -6,91 +6,64 @@ class SignIn extends Component {
   constructor(props) {
     super(props);
 
-        this.state = {
-            username: "",
-            password: "",
-            loginError: "",
-            users: [],
-            error: null
-        };
-        
-        this.handleChange = this.handleChange.bind(this);
-        this.submitLogin = this.submitLogin.bind(this);
-    }
+    this.state = {
+      username: "",
+      password: "",
+      loginError: "",
+      users: [],
+      error: null
+    };
 
-    handleChange = (e) => {
-        this.setState({
-            users: this.props.userList,
-            [e.target.id]: e.target.value
-        });
-        console.log(this.state);
-    }
-
-    submitLogin(e) {
-        e.preventDefault();
-        const {username, password} = this.state;
-        this.state.users.map(user => {
-            if(user.username === this.state.username && user.password === this.state.password){
-                this.props.handleSuccessfulAccount(user);
-            }else if(user.username !== this.state.username){
-                this.setState({loginError: "Account does not exist."}) //fix bug
-            }else if(user.username === this.state.username && user.password !== this.state.password){
-                this.setState({loginError: "Incorrect password."})
-            }
-        })
-        console.log(this.state);
-    }
-
+    this.handleChange = this.handleChange.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+  }
 
   handleChange = e => {
     this.setState({
+      users: this.props.userList,
       [e.target.id]: e.target.value
     });
+    console.log(this.state);
   };
 
   submitLogin(e) {
     e.preventDefault();
     const { username, password } = this.state;
     this.state.users.map(user => {
-      if (user.username === this.state.username) {
-        if (user.password === this.state.password) {
-          this.props.handleSuccessfulAccount(user);
-        } else {
-          this.setState({ loginError: "Incorrect password." }); //fix bug
-        }
-      } else {
-        this.setState({ loginError: "Account does not exist." });
+      if (
+        user.username === this.state.username &&
+        user.password === this.state.password
+      ) {
+        this.props.handleSuccessfulAccount(user);
+      } else if (user.username !== this.state.username) {
+        this.setState({ loginError: "Account does not exist." }); //fix bug
+      } else if (
+        user.username === this.state.username &&
+        user.password !== this.state.password
+      ) {
+        this.setState({ loginError: "Incorrect password." });
       }
     });
     console.log(this.state);
   }
 
-                <form className="box" onSubmit={this.submitLogin}>
-                
-                    <div className="input-group">
-                        <label htmlFor="username">Username</label>
-                        <input 
-                            type="text" 
-                            id="username" 
-                            onChange={this.handleChange}
-                            required
-                            className="login-input" 
-                            placeholder="Username"/>
-                    </div>
+  render() {
+    return (
+      <div className="inner-container">
+        <div className="header">Sign In</div>
 
-                    <div className="input-group">
-                        <label htmlFor="password">Password</label>
-                        <input 
-                            type="password" 
-                            id="password" 
-                            onChange={this.handleChange}
-                            required
-                            className="login-input" 
-                            placeholder="Password"/>
-                    </div>
-                    {this.state.loginError}
-                    <button type="submit" className="login-btn" onClick={this.submitLogin}>Sign In</button>
-
+        <form className="box" onSubmit={this.submitLogin}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={this.handleChange}
+              required
+              className="login-input"
+              placeholder="Username"
+            />
+          </div>
 
           <div className="input-group">
             <label htmlFor="password">Password</label>
@@ -105,7 +78,7 @@ class SignIn extends Component {
           </div>
           {this.state.loginError}
           <button
-            type="button"
+            type="submit"
             className="login-btn"
             onClick={this.submitLogin}
           >
